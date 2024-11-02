2ccdf5ce566b540e8b8bd4abbc085d4c6bd0ff14
Roberto <rd.navaro@gmail.com>
2024-11-02 18:30:06 -0300

    removedups

--- LinkedLists/src/test/java/_01_remove_dups/RemoveDupsTest.java
+++ LinkedLists/src/test/java/_01_remove_dups/RemoveDupsTest.java
@@ -1,5 +1,23 @@
 package _01_remove_dups;
 
 public class RemoveDupsTest {
+   
+  @Test
+  public void removeDupsTest() {
+    RemoveDups removeDups = new RemoveDups();
+    Node head = new Node(1);
+    head.next = new Node(2);
+    head.next.next = new Node(2);
+    head.next.next.next = new Node(3);
+    head.next.next.next.next = new Node(4);
+    head.next.next.next.next.next = new Node(1);
 
+    removeDups.removeDups(head);
+
+    assertEquals(1, head.value);
+    assertEquals(2, head.next.value);
+    assertEquals(3, head.next.next.value);
+    assertEquals(4, head.next.next.next.value);
+    assertEquals(null, head.next.next.next.next);
+  }
 }
