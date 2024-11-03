a888e3d8c12d43cc2ee85028802b81d1d4ee5b98
Roberto <rd.navaro@gmail.com>
2024-11-03 16:38:29 -0300

    mergetwosortedlist

--- LinkedLists/src/test/java/_02_merge_two_sorted_list/MergeTwoSortedListsTest.java
+++ LinkedLists/src/test/java/_02_merge_two_sorted_list/MergeTwoSortedListsTest.java
@@ -2,4 +2,25 @@ package _02_merge_two_sorted_list;
 
 public class MergeTwoSortedListsTest {
 
+    @Test
+  public void mergeTwoSortedListsTest() {
+    MergeTwoSortedLists merge = new MergeTwoSortedLists();
+    Node list1 = new Node(1);
+    list1.next = new Node(2);
+    list1.next.next = new Node(4);
+    list1.next.next.next = new Node(6);
+
+    Node list2 = new Node(2);
+    list2.next = new Node(3);
+    list2.next.next = new Node(5);
+
+    Node result = merge.mergeTwoLists(list1, list2);
+    assertEquals(1, result.value);
+    assertEquals(2, result.next.value);
+    assertEquals(2, result.next.next.value);
+    assertEquals(3, result.next.next.next.value);
+    assertEquals(4, result.next.next.next.next.value);
+    assertEquals(5, result.next.next.next.next.next.value);
+    assertEquals(6, result.next.next.next.next.next.next.value);
+  }
 }
