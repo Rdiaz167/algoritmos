88855fca6297471de300e2b0c3d0016db3bb7f1b
Roberto <rd.navaro@gmail.com>
2024-11-11 20:58:22 -0300

    stackmin

---
+++ StacksQueues/src/main/test/_04_stack_min/StackMin.java
@@ -0,0 +1,24 @@
+public class StackMin {
+
+    private Deque<Integer> valuesStack = new ArrayDeque<>();
+    private Deque<Integer> minStack = new ArrayDeque<>();
+
+    public void push(Integer data) {
+        valuesStack.push(data);
+        if(minStack.isEmpty() || data < minStack.peek()) {
+            minStack.push(data);
+        }
+    }
+
+    public int pop() {
+        int value = valuesStack.pop();
+        if(value == minStack.peek()) {
+            minStack.pop();
+        }
+        return value;
+    }
+
+    public int min() {
+        return minStack.peek();
+    }
+}
