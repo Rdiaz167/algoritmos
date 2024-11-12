a54a6a4ea01138fb15861e1e8083281d293029a0
Roberto <rd.navaro@gmail.com>
2024-11-11 21:00:09 -0300

    stackmin2

--- StacksQueues/src/main/test/_04_stack_min/StackMin.java
+++ StacksQueues/src/main/test/_04_stack_min/StackMin.java
@@ -1,6 +1,12 @@
 
 import java.util.Deque;
 
+
+/*
+ * ¿Cómo diseñarías un Stack que además de las operaciones de push y pop también
+ * contase con una operación para obtener el mínimo?
+ */
+
 public class StackMin {
 
     private Deque<Integer> valuesStack = new ArrayDeque<>();
