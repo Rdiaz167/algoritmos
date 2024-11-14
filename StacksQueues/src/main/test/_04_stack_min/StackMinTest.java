b599ce1f8a3174e5638ee015e039fdc1c19bac09
Roberto <rd.navaro@gmail.com>
2024-11-14 19:43:54 -0300

    stackmintest1

--- StacksQueues/src/main/test/_04_stack_min/StackMinTest.java
+++ StacksQueues/src/main/test/_04_stack_min/StackMinTest.java
@@ -1,17 +1,19 @@
 public class StackMinTest {
 
-    StackMin stackMin = new StackMin();
+    @Test
+    public void stackMinTest() {
+        StackMin stackMin = new StackMin();
 
-    stackMin.push(3);
-    stackMin.push(1);
-    stackMin.push(2);
+        stackMin.push(3);
+        stackMin.push(1);
+        stackMin.push(2);
 
-    assertEquals(1, stackMin.min());
-    stackMin.pop();
-    assertEquals(1, stackMin.min());
-    stackMin.pop();
-    assertEquals(3, stackMin.min());
-    stackMin.push(0);
-    assertEquals(0, stackMin.min());
+        assertEquals(1, stackMin.min());
+        stackMin.pop();
+        assertEquals(1, stackMin.min());
+        stackMin.pop();
+        assertEquals(3, stackMin.min());
+        stackMin.push(0);
+        assertEquals(0, stackMin.min());
   }
 }
