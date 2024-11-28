d544daff56b90db9cf709ce20e21b5713f1e00a4
Roberto <rd.navaro@gmail.com>
2024-11-28 20:14:37 -0300

    addtwonumberstest2

--- StacksQueues/src/main/test/_03_sort_stack/SortStackTest.java
+++ StacksQueues/src/main/test/_03_sort_stack/SortStackTest.java
@@ -2,6 +2,7 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class SortStackTest {
