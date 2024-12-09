9ee8217a565eea358c82d9c7893aed4042445ac9
Roberto <rd.navaro@gmail.com>
2024-12-08 21:20:45 -0300

    clean7

--- ArraysStringsHashTables/src/main/java/_03_group_anagrams/GroupAnagrams.java
+++ ArraysStringsHashTables/src/main/java/_03_group_anagrams/GroupAnagrams.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 // Un anagrama es una palabra creada a partir de la reordenación de las letras de otra palabra. Ej: saco - caso
-// Dado un array de strings, devuelve los anagramas agrupados. Cualquier orden es válido.
+// Dado un array de strings, devuelve  los anagramas agrupados. Cualquier orden es válido.
  
 // Ejemplo:
 // Input: words = ["saco", "arresto", "programa", "rastreo", "caso"]
