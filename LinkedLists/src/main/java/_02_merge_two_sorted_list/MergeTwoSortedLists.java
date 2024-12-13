98185228f8b941f703397e114071b8337c735a7f
Roberto <rd.navaro@gmail.com>
2024-12-12 22:04:38 -0300

    clean15

--- LinkedLists/src/main/java/_02_merge_two_sorted_list/MergeTwoSortedLists.java
+++ LinkedLists/src/main/java/_02_merge_two_sorted_list/MergeTwoSortedLists.java
@@ -7,7 +7,7 @@ import _00_linkedlist.Node;
  * El resultado debe ser una única  lista enlazada ordenada. Devuelve su head.
  *
  * Ejemplo:
- *  Input: 1->2->4->6, 2->3->5
+ *  Input: 1->2->4->6,  2->3->5
  *  Output: 1->2->2->3->4->5->6
  */
 
