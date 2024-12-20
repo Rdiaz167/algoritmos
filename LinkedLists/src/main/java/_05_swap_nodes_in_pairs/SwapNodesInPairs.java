1708d68d6925bfdc23a95c43a46e41fb37c72e41
Roberto <rd.navaro@gmail.com>
2024-12-19 23:21:45 -0300

    clean25

--- LinkedLists/src/main/java/_05_swap_nodes_in_pairs/SwapNodesInPairs.java
+++ LinkedLists/src/main/java/_05_swap_nodes_in_pairs/SwapNodesInPairs.java
@@ -6,7 +6,7 @@ import _00_linkedlist.Node;
  * Escribe un algoritmo que intercambie parejas de nodos adyacentes sin
  * modificar el valor interno de los nodos .
  *
- * Ejemplo:
+ * Ejemplo :
  *  Input: 1->2->4->6->8
  *  Output: 2->1->6->4->8
  */
