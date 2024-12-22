10880e7a1737c505b625ab7bda8423c9cfb5d1e3
Roberto <rd.navaro@gmail.com>
2024-12-21 22:47:51 -0300

    clean26

--- StacksQueues/src/main/java/_01_queue_with_stacks/QueueWithStacks.java
+++ StacksQueues/src/main/java/_01_queue_with_stacks/QueueWithStacks.java
@@ -4,7 +4,7 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 // * Como Stack usaremos la Interfaz Deque, que proporciona las operaciones de una cola doblemente terminada,
-// * permitiéndola usar como Pila o Cola (solo las operaciones de Pila están permitidas en este ejercicio).
+// * permitiéndola usar como Pila o Cola (solo las operaciones de Pila están permitidas en este ejercicio) .
 
 public class QueueWithStacks {
 
