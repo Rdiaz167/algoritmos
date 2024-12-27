df3a25f99fec6353b1f575ce92a72b3f42547a7d
Roberto <rd.navaro@gmail.com>
2024-12-27 20:28:16 -0300

    test10

--- StacksQueues/src/main/java/_04_stack_min/StackMin.java
+++ StacksQueues/src/main/java/_04_stack_min/StackMin.java
@@ -5,7 +5,7 @@ import java.util.Deque;
 
 /*
  * ¿Cómo diseñarías un  Stack que además de las operaciones de push y pop también
- * contase con una operación para obtener el mínimo ?
+ *  contase con una operación para  obtener el mínimo ?
  */
 
 public class StackMin {
